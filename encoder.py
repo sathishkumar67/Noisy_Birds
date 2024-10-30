54427c93dfe2a27c050b9121d65b1379a5e76b51
sathishkumar67 <sathishvillers2000@gmail.com>
2024-10-30 13:59:50 +0530

    updated

--- encoder.py
+++ encoder.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from dataclasses import dataclass
-from typing import Optional, Tuple
+from typing import Tuple
 
 
 @dataclass
@@ -72,7 +72,6 @@ class EncoderAttention(nn.Module):
         self.embed_dim = config.hidden_size
         self.num_heads = config.num_attention_heads
         self.head_dim = self.embed_dim // self.num_heads
-        self.scale = self.head_dim**-0.5 # Equivalent to 1 / sqrt(self.head_dim)
         self.dropout = config.attention_dropout
 
         self.k_proj = nn.Linear(self.embed_dim, self.embed_dim)
@@ -80,54 +79,23 @@ class EncoderAttention(nn.Module):
         self.q_proj = nn.Linear(self.embed_dim, self.embed_dim)
         self.out_proj = nn.Linear(self.embed_dim, self.embed_dim)
 
-    def forward(
-        self,
-        hidden_states: torch.Tensor,
-    ) -> Tuple[torch.Tensor, Optional[torch.Tensor]]:
+    def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
+        B, T, C = hidden_states.shape
 
-        # hidden_states: [Batch_Size, Num_Patches, Embed_Dim]
-        batch_size, seq_len, _ = hidden_states.size()
-        # query_states: [Batch_Size, Num_Patches, Embed_Dim]
-        query_states = self.q_proj(hidden_states)
-        # key_states: [Batch_Size, Num_Patches, Embed_Dim]
-        key_states = self.k_proj(hidden_states)
-        # value_states: [Batch_Size, Num_Patches, Embed_Dim]
-        value_states = self.v_proj(hidden_states)
-        # query_states: [Batch_Size, Num_Heads, Num_Patches, Head_Dim]
-        query_states = query_states.view(batch_size, seq_len, self.num_heads, self.head_dim).transpose(1, 2)
+        # query, key, value projections
+        q, k, v = self.q_proj(hidden_states), self.k_proj(hidden_states), self.v_proj(hidden_states)
+       
+        q = q.view(B, T, self.num_heads, C // self.num_heads).transpose(1, 2) 
+        k = k.view(B, T, self.num_heads, C // self.num_heads).transpose(1, 2) 
+        v = v.view(B, T, self.num_heads, C // self.num_heads).transpose(1, 2) 
+        
+        y = F.scaled_dot_product_attention(q, k, v, is_causal=False) # flash attention
+        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+        
+        # output projection
+        y = self.out_proj(y)
+        return y
 
-        key_states = key_states.view(batch_size, seq_len, self.num_heads, self.head_dim).transpose(1, 2)
-
-        value_states = value_states.view(batch_size, seq_len, self.num_heads, self.head_dim).transpose(1, 2)
-        # Calculate the attention using the formula Q * K^T / sqrt(d_k). attn_weights: [Batch_Size, Num_Heads, Num_Patches, Num_Patches]
-        attn_weights = (torch.matmul(query_states, key_states.transpose(2, 3)) * self.scale)
-
-        if attn_weights.size() != (batch_size, self.num_heads, seq_len, seq_len):
-            raise ValueError(
-                f"Attention weights should be of size {(batch_size, self.num_heads, seq_len, seq_len)}, but is"
-                f" {attn_weights.size()}"
-            )
-
-        # Apply the softmax row-wise. attn_weights: [Batch_Size, Num_Heads, Num_Patches, Num_Patches]
-        attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query_states.dtype)
-        # Apply dropout only during training
-        attn_weights = nn.functional.dropout(attn_weights, p=self.dropout, training=self.training)
-        # Multiply the attention weights by the value states. attn_output: [Batch_Size, Num_Heads, Num_Patches, Head_Dim]
-        attn_output = torch.matmul(attn_weights, value_states)
-
-        if attn_output.size() != (batch_size, self.num_heads, seq_len, self.head_dim):
-            raise ValueError(
-                f"`attn_output` should be of size {(batch_size, self.num_heads, seq_len, self.head_dim)}, but is"
-                f" {attn_output.size()}"
-            )
-        # [Batch_Size, Num_Heads, Num_Patches, Head_Dim] -> [Batch_Size, Num_Patches, Num_Heads, Head_Dim]
-        attn_output = attn_output.transpose(1, 2).contiguous()
-        # [Batch_Size, Num_Patches, Num_Heads, Head_Dim] -> [Batch_Size, Num_Patches, Embed_Dim]
-        attn_output = attn_output.reshape(batch_size, seq_len, self.embed_dim)
-        # [Batch_Size, Num_Patches, Embed_Dim]
-        attn_output = self.out_proj(attn_output)
-
-        return attn_output, attn_weights
 
 
 class EncoderMLP(nn.Module):
@@ -141,7 +109,7 @@ class EncoderMLP(nn.Module):
         # [Batch_Size, Num_Patches, Embed_Dim] -> [Batch_Size, Num_Patches, Intermediate_Size]
         hidden_states = self.fc1(hidden_states)
         # hidden_states: [Batch_Size, Num_Patches, Intermediate_Size]
-        hidden_states = nn.functional.gelu(hidden_states, approximate="tanh")
+        hidden_states = F.gelu(hidden_states, approximate="tanh")
         # [Batch_Size, Num_Patches, Intermediate_Size] -> [Batch_Size, Num_Patches, Embed_Dim]
         hidden_states = self.fc2(hidden_states)
 
