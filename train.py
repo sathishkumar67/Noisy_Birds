2b8a7a4d3d15585f05026c7489e6e05d970c1021
sathishkumar67 <sathishvillers2000@gmail.com>
2024-11-07 12:39:54 +0530

    updated

--- train.py
+++ train.py
@@ -1,8 +1,20 @@
 from __future__ import annotations
+import gin
 import torch
-import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
-import gin
 import lightning as L
+
+from dataset import Birddataset
+from encoder import EncoderConfig, EncoderModel
+from decoder import DecoderConfig, DecoderModel
+
+
+def main():
+    train_dataset = Birddataset("dataset", ["budgie", "canary", "duckling", "rubber duck", "unlabeled"], "train")
+    test_dataset = Birddataset("dataset", ["budgie", "canary", "duckling", "rubber duck", "unlabeled"], "test")
+
+
+if __name__ == '__main__':
+    main()
