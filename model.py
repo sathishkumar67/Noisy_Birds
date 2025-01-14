aa5cbbb11eba5615c8128eae9da816a941f84566
sathishkumar67 <sathishvillers2000@gmail.com>
2025-01-14 15:24:32 +0530

    prepared a wrapper class for encoder and decoder

---
+++ model.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+from typing import Tuple
+import gin
+import torch
+import torch.nn as nn
+import lightning as L
+from dataclasses import dataclass
+from mae.encoder import *
+from mae.decoder import *
+
+@gin.configurable
+@dataclass
+class MAEWrapperConfig:
+    lr: float
+    batch_size: int
+    num_epochs: int
+    weight_decay: float
+    eps: float
+    seed: int
+    betas: Tuple[float, float]
+    gpu_count: int
+
+
+class MAEWrapper(L.LightningModule):
+    def __init__(self, encoder: nn.Module, decoder: nn.Module, config: MAEWrapperConfig):
+        super().__init__()
+        self.config = config
+        self.encoder = encoder
+        self.decoder = decoder
+        self.optimizer = self.configure_optimizers()
+
+    def training_step(self, batch, batch_idx):
+        self.model.train()
+        optimizer = self.optimizers()
+        optimizer.zero_grad()
+        
+        batch, label = batch
+        encoder_op, mask, ids_restore = self.encoder(batch)
+        decoder_op = self.decoder((encoder_op, mask, ids_restore), batch)
+        loss = decoder_op[1] 
+        self.log("Train_Loss", loss, prog_bar=True)
+
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.AdamW(self.model.parameters(), lr=self.config.lr, betas=self.config.betas, eps=self.config.eps, weight_decay=self.config.weight_decay)
+        return optimizer
