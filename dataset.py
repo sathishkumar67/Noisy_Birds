e05f20e25e9c3dcd28dfc8f0cc6693cd0d5d4c73
sathishkumar67 <sathishvillers2000@gmail.com>
2025-02-17 10:41:13 +0530

    removed normalisation for images

--- dataset.py
+++ dataset.py
@@ -25,10 +25,7 @@ class Birddataset(Dataset):
         self.train_samples = []
         self.test_samples = []
 
-        self.transform = transforms.Compose([
-                    transforms.ToTensor(),
-                    transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                    std=[0.229, 0.224, 0.225])])
+        self.transform = transforms.ToTensor()
 
         # Scan directories only once
         with ThreadPoolExecutor() as executor:
